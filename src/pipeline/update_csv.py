a60ddcc69d3aed6b9cacf0b81d69e5e0ba1e8e1d
pablotorao <pmanning@uninorte.edu.co>
2025-11-30 17:24:55 -0500

    en teoria funciona

--- src/pipeline/update_csv.py
+++ src/pipeline/update_csv.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import json
 from datetime import datetime, timedelta
+import requests
 # --- 1. JSON de entrada ---
 data_json = {
     "success": True,
     "message": "Historicos de Cartagena entontrados",
     "data": [
         {
-            "fecha": "2019-06-29T05:00:00.000Z",
+            "fecha": "2025-11-04T05:00:00.000Z",
             "p1": 426.4, "p2": 413.6, "p3": 403, "p4": 390.3, "p5": 382.7, "p6": 370.8,
             "p7": 356.1, "p8": 366, "p9": 387.7, "p10": 405.9, "p11": 415.7, "p12": 419.4,
             "p13": 419.1, "p14": 421.3, "p15": 423.2, "p16": 419.3, "p17": 408, "p18": 398.9,
@@ -15,7 +16,7 @@ data_json = {
             "observacion": ""
         },
         {
-            "fecha": "2019-06-30T05:00:00.000Z",
+            "fecha": "2025-11-05T05:00:00.000Z",
             "p1": 414.9, "p2": 400.7, "p3": 385.6, "p4": 380, "p5": 370.4, "p6": 357.9,
             "p7": 338.7, "p8": 339, "p9": 347.8, "p10": 350.7, "p11": 360.5, "p12": 364.1,
             "p13": 363.4, "p14": 369, "p15": 370.1, "p16": 357.2, "p17": 358.2, "p18": 359.6,
@@ -23,7 +24,7 @@ data_json = {
             "observacion": ""
         },
         {
-            "fecha": "2019-07-01T05:00:00.000Z",
+            "fecha": "2025-11-06T05:00:00.000Z",
             "p1": 394.6, "p2": 381.3, "p3": 373.8, "p4": 366, "p5": 360.2, "p6": 351.1,
             "p7": 340, "p8": 334.6, "p9": 339.4, "p10": 345, "p11": 353.3, "p12": 358.6,
             "p13": 359.8, "p14": 364.4, "p15": 370.6, "p16": 359.7, "p17": 342.6, "p18": 337,
@@ -35,8 +36,9 @@ data_json = {
 
 # --- 2. Convertir JSON a DataFrame ---
 def json_to_csv_power(data_json,ucp_name,variable="Demanda_Real",clasificador="NORMAL"):
+    archivo='../../data/raw/historicos_cartagena.csv'
     df = pd.DataFrame(data_json["data"])
-
+    
     # --- 3. Convertir fecha a YYYY-MM-DD ---
     df["FECHA"] = pd.to_datetime(df["fecha"]).dt.date
     df.drop(columns=["fecha"], inplace=True)
@@ -59,14 +61,21 @@ def json_to_csv_power(data_json,ucp_name,variable="Demanda_Real",clasificador="N
 
     # --- 8. Reordenar columnas como tu CSV ---
     final_cols = ['UCP', 'VARIABLE', 'FECHA', 'Clasificador interno', 'TIPO DIA'] + cols_p + ["TOTAL"]
-    df = df[final_cols]
+    df2 = df[final_cols]
+    #df2 el generado del json, se le pega a df1 que es el historico original
+    df1=pd.read_csv(archivo)
+    df_final = pd.concat([df1, df2], axis=0, ignore_index=True)
 
     # --- 7. Guardar CSV ---
-    df.to_csv("../../data/raw/historicos_cartagena.csv", index=False)
+    df_final.to_csv(archivo, index=False)
 
     print("CSV generado correctamente.")
 
-#df=pd.read_csv('/Users/pablo/Documents/GitHub/EPM/string/data_with_features_latest.csv')
-df=pd.read_csv('../../data/raw/datos.csv')
-ultima_fecha=df['FECHA'].max()
-print(ultima_fecha)   
+
+def regresar_nuevo_csv(ucp):
+    df=pd.read_csv('../../data/raw/datos.csv')
+    ultima_fecha=df['FECHA'].max()  
+    base_url = "http://localhost:8000"
+    url = f"{base_url}/cargarPeriodosxUCPDesdeFecha/{ucp}/{ultima_fecha}"
+    response = requests.get(url)
+    json_to_csv_power(response.json(),ucp_name=ucp)
